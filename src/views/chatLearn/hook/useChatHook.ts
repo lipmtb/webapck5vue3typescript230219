5e8ede23f719bee1869de51629cb63db3236b44a
lipmtb <littpm@163.com>
2023-09-03 18:29:25 +0800

    修改回turb35接口

--- src/views/chatLearn/hook/useChatHook.ts
+++ src/views/chatLearn/hook/useChatHook.ts
@@ -1,6 +1,6 @@
-import { ref, computed, reactive } from "vue";
+import {  ref, computed, reactive } from "vue";
 import { useStore } from "vuex";
-import longTurnAi from "@/service/longTurnAi";
+import chatai from "@/service/chatai";
 
 export type TChatContent = {
     userId: string;
@@ -22,33 +22,9 @@ const useChatHook = () => {
     const userName = computed(() => {
         return store.getters["userArea/getUserName"];
     })
-    const promptText = computed(() => {
-        return store.getters["longTextArea/getPromptText"];
-    })
-    const promptTurnLen = computed(() => {
-        return store.getters["longTextArea/getTurnLen"];
-    })
-    const maxTurnCheck = async () => {
-        if (promptTurnLen?.value < 100) {
-            return true;
-        }
-        const isClear = confirm("是否清除回话");
-        if (isClear) {
-            store.dispatch("longTextArea/clearPromptAction")
-            return false;
-        } else {
-            return true;
-        }
-
-    }
     // 发送聊天
     const onSubmitChat = async (reqText: string, apiKey: string) => {
-        const isContinue = await maxTurnCheck();
-        if (!isContinue) {
-            return;
-        }
         state.loading = true;
-        const humanText = promptText?.value ? promptText?.value + "\nHuman: " + reqText : "Human: " + reqText;
         state.chatList.push({
             userId: userId?.value,
             userName: userName?.value,
@@ -57,19 +33,14 @@ const useChatHook = () => {
         })
         try {
             // 发送给后台服务处理
-            // const aiResponse = await chatai({ userId: userId?.value, content: reqText, apiKey });
-            const aiResponse = await longTurnAi({ userId: userId?.value, content: humanText, apiKey });
-            const message = aiResponse?.choices?.[0]?.text;
+            const aiResponse = await chatai({ userId: userId?.value, content: reqText, apiKey });
+            const message = aiResponse?.choices?.[0]?.message?.content;
             // const message = require("@/mock/aiRes.json");// mock
             state.chatList.push({
                 userId: "assistant",
                 userName: "bot",
                 time: new Date().toLocaleTimeString(),
-                content: message?.replace(/^\n*.+[：|:]/, "")
-            })
-            // 记录多轮对话
-            store.dispatch("longTextArea/setNewPromptAction", {
-                text: humanText + message
+                content: message ?? ""
             })
         } catch (err) {
             console.error("发送失败", err);
