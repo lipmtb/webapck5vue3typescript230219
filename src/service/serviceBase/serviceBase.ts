e1e3fa07071618d5abe98b8ce8ec0a7f3cddc66b
lipmtb <littpm@163.com>
2024-02-11 11:52:46 +0800

    axios baseurl

--- src/service/serviceBase/serviceBase.ts
+++ src/service/serviceBase/serviceBase.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
-export const ServerBaseUrl = "http://127.0.0.1:82";
+import axios from 'axios';
+export const ServerBaseUrl = "/jjcc";
 
 const serviceBaseAxios = axios.create({
     baseURL: ServerBaseUrl,
